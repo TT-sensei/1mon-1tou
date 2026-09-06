05c268284d3a12f232c4ffe13aa2ce7233c6290d
TT-sensei <rlrieaeoy@gmail.com>
2026-09-07 06:29:35 +0900

    プレイヤーカードのバトル用データを独立化

---
+++ data/cards.js
@@ -0,0 +1,6 @@
+// 教科モンスターバトル：プレイヤーカードの最低限のバトル表示定義
+window.BATTLE_CARD_FALLBACKS = [
+  {id:'starter-slime',name:'ぷるんスライム',src:'assets/monsters/purun-slime.svg'},
+  {id:'starter-bat',name:'こもりんバット',src:'assets/monsters/komorin-bat.svg'},
+  {id:'starter-kinoko',name:'きのこっこ',src:'assets/monsters/kinoko.svg'}
+];
