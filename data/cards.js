eeb22ee396683737cf2ffe7956bad9a82a32f4e5
TT-sensei <rlrieaeoy@gmail.com>
2026-09-21 10:59:30 +0900

    ナビアンをモンスターカードへ復元

--- data/cards.js
+++ data/cards.js
@@ -1,11 +1,14 @@
-// 教科モンスターバトル：ナビアン（ナビキャラ）
-const NAVI_BASE='https://tt-sensei.github.io/navi-character-/assets/web/characters';
-window.BATTLE_CARDS=[
-{id:'riku',name:'りく',subject:'国語',rarity:'normal',naviSrc:NAVI_BASE+'/riku/expressions/01-normal-smile.webp'},
-{id:'sora',name:'そら',subject:'算数',rarity:'normal',naviSrc:NAVI_BASE+'/sora/expressions/01-normal-smile.webp'},
-{id:'kai',name:'かい',subject:'理科',rarity:'rare',naviSrc:NAVI_BASE+'/kai/expressions/01-normal-smile.webp'},
-{id:'saku',name:'さく',subject:'社会',rarity:'normal',naviSrc:NAVI_BASE+'/saku/expressions/01-normal-smile.webp'},
-{id:'tsuki',name:'つき',subject:'英語',rarity:'rare',naviSrc:NAVI_BASE+'/tsuki/expressions/01-normal-smile.webp'},
-{id:'nami',name:'なみ',subject:'生活',rarity:'normal',naviSrc:NAVI_BASE+'/nami/expressions/01-normal-smile.webp'}
+// 教科モンスターバトル：カード定義
+// カード本体はモンスター定義から独立。背景・フレーム・表示素材を後から差し替えられる。
+window.BATTLE_CARDS = [
+  {id:'purun-slime',name:'ぷるんスライム',monsterId:'purun-slime',rarity:'normal'},
+  {id:'komorin-bat',name:'こもりんバット',monsterId:'komorin-bat',rarity:'normal'},
+  {id:'kinoko',name:'きのこっこ',monsterId:'kinoko',rarity:'normal'},
+  {id:'cave-wolf',name:'ケイブウルフ',monsterId:'cave-wolf',rarity:'rare'},
+  {id:'forest-goblin',name:'フォレストゴブリン',monsterId:'forest-goblin',rarity:'rare'},
+  {id:'candy-slug',name:'キャンディスラッグ',monsterId:'candy-slug',rarity:'normal'},
+  {id:'cogwheel',name:'コグホイール',monsterId:'cogwheel',rarity:'rare'},
+  {id:'slime-king',name:'スライムキング',monsterId:'slime-king',rarity:'boss'},
+  {id:'flame-dragon',name:'フレイムドラゴン',monsterId:'flame-dragon',rarity:'boss'}
 ];
 window.getBattleCard=function(id){return (window.BATTLE_CARDS||[]).find(card=>card.id===id)||null};
